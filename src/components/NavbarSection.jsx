fd6980db3d1a201070bca16d7de592749e8d02eb
HassaanKhan10 <109235303+HassaanKhan10@users.noreply.github.com>
2025-12-03 15:19:33 +0500

    update

--- src/components/NavbarSection.jsx
+++ src/components/NavbarSection.jsx
@@ -1,85 +1,58 @@
 import React from "react";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function NavbarSection() {
   return (
-    <div>
-      <section className="nav-section py-3 px-3 position-fixed top-0 start-0 end-0" data-aos-duration="300"  data-aos="fade-down">
-        <div className="container py-2 py-lg-0">
-          <nav className="navbar navbar-expand-lg py-0 ">
-            <a className="navbar-brand me-0 px-3" href="/">
-              <img src="assets/images/logo.png" alt="logo" />
-            </a>
-            <button
-              className="navbar-toggler border-0 px-0"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse justify-content-end"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav px-lg-4 py-3 pb-0 py-lg-0">
-                <li className="nav-item">
-                  <a
-                    className="nav-link home-nav text-grey text-hover-white active"
-                    href="#hero-section"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link text-grey text-hover-white"
-                    href="#about-us"
-                  >
-                    About us
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link text-grey text-hover-white"
-                    href="#services"
-                  >
-                    Services
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link text-grey text-hover-white"
-                    href="#locations"
-                  >
-                    Gallery
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link text-grey text-hover-white"
-                    href="#contact"
-                  >
-                    FAQ
-                  </a>
-                </li>
-              </ul>
-              <div className="d-flex align-items-center">
-                <a
-                  href="#about-us"
-                  className="btn btn-primary me-3"
-                >
-                  Get a Quote
-                </a>
-              </div>
+    <section
+      className="nav-section py-3 px-3 position-fixed top-0 start-0 end-0"
+      data-aos-duration="300"
+      data-aos="fade-down"
+    >
+      <Container className="py-2 py-lg-0">
+        <Navbar expand="lg" collapseOnSelect className="py-0">
+          {/* Logo */}
+          <Navbar.Brand href="/" className="me-0 px-3">
+            <img src="assets/images/logo.png" alt="logo" />
+          </Navbar.Brand>
+
+          {/* Toggler */}
+          <Navbar.Toggle aria-controls="main-navbar" />
+
+          {/* Menu Items */}
+          <Navbar.Collapse id="main-navbar" className="justify-content-end">
+            <Nav className="px-lg-4 py-3 pb-0 py-lg-0">
+              <Nav.Link href="#hero-section" className="home-nav text-grey text-hover-white">
+                Home
+              </Nav.Link>
+
+              <Nav.Link href="#about-us" className="text-grey text-hover-white">
+                About us
+              </Nav.Link>
+
+              <Nav.Link href="#services" className="text-grey text-hover-white">
+                Services
+              </Nav.Link>
+
+              <Nav.Link href="#locations" className="text-grey text-hover-white">
+                Gallery
+              </Nav.Link>
+
+              <Nav.Link href="#contact" className="text-grey text-hover-white">
+                FAQ
+              </Nav.Link>
+            </Nav>
+
+            {/* Button */}
+            <div className="d-flex align-items-center ms-lg-3">
+              <a href="#about-us" className="btn btn-primary me-3">
+                Get a Quote
+              </a>
             </div>
-          </nav>
-        </div>
-      </section>
-    </div>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </section>
   );
 }
 
